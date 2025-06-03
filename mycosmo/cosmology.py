c39b40f872bbb99f5b6c44995d564645523d3fa2
Leonor Simoes <leonor@client-172-18-93-209.eduroam.universite-paris-saclay.fr>
2025-06-03 10:38:29 +0200

    Refactored code, added constants.py module

--- mycosmo/cosmology.py
+++ mycosmo/cosmology.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+from .constants import Mpc, G
+
+
+
 def hubble(redshift, cosmo_dict):
     hubble_const = cosmo_dict["H0"]
     matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
@@ -10,8 +16,7 @@ def hubble(redshift, cosmo_dict):
 
 
 def critical_density(redshift, cosmo_dict):
-    Mpc = 3.08568e22
-    G = 6.6743e-11
     H_z_si = hubble(redshift, cosmo_dict) * 1e3 / Mpc
 
     return (3.0 * H_z_si**2) / (8.0 * np.pi * G)
+
